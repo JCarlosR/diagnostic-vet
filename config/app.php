da2309beee9a16cf42b984885ca7774965d2f5b1
Juan Carlos <juancagb.17@gmail.com>
2017-03-13 18:12:29 -0500

    Change app name

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Diagnostico',
+    'name' => 'Diagnostic Vet',
 
     /*
     |--------------------------------------------------------------------------
